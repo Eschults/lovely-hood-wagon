023fcc50e5cd174463c1d7f12950ad1dbb6e7627
Edward Schults <edward.schults@gmail.com>
2014-12-08 23:19:42 +0100

    added price column to offer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208215617) do
+ActiveRecord::Schema.define(version: 20141208221846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141208215617) do
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
+    t.integer  "price"
   end
 
   add_index "offers", ["user_id"], name: "index_offers_on_user_id", using: :btree

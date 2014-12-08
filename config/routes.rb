5a4096e25947f7bd529d28e87f8fef3e3c73fedf
Edward Schults <edward.schults@gmail.com>
2014-12-08 12:40:54 +0100

    fb connect migration on User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "pages#home"
   # devise_for :users
   # resources :users, only: [:show, :edit, :update]

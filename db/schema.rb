876394df99363c848becab2c87b3ff148200c970
Edward Schults <edward.schults@gmail.com>
2014-12-09 13:18:04 +0100

    migration corrected

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209121240) do
+ActiveRecord::Schema.define(version: 20141209121646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141209121240) do
     t.datetime "picture_updated_at"
     t.integer  "price"
     t.boolean  "public"
+    t.string   "type_of_offer"
   end
 
   add_index "offers", ["user_id"], name: "index_offers_on_user_id", using: :btree

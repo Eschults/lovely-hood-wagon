18c9fe9015384be5c03baff29e5c148d72aa5fb3
Edward Schults <edward.schults@gmail.com>
2014-12-10 15:03:47 +0100

    front on offers#index

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,19 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record == user
+  end
+end

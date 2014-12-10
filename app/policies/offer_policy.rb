0e3a845903da0b9958efc4408226a872c5c444ba
Edward Schults <edward.schults@gmail.com>
2014-12-10 10:39:37 +0100

    last checkpoint before test on offers.js

--- app/policies/offer_policy.rb
+++ app/policies/offer_policy.rb
@@ -4,4 +4,20 @@ class OfferPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true  # Anyone can view an offer
+  end
+
+  def create?
+    true  # Anyone can create an offer
+  end
+
+  def update?
+    record.user == user  # Only offer creator can update it
+  end
+
+  # def destroy?
+  #   record.user == user  # Only offer creator can destroy it
+  # end
 end

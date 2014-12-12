91319c1d5cd36e8d4c7e5cade13a8218b0f6ad8b
Edward Schults <edward.schults@gmail.com>
2014-12-12 11:36:22 +0100

    changed attribute name pulled to fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,8 @@ class User < ActiveRecord::Base
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]  # Fake password for validation
-      user.name = auth.info.name
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.picture = URI.parse(auth.info.image.gsub("­http","htt­ps"))
       user.token = auth.credentials.token
       user.token_expiry = Time.at(auth.credentials.expires_at)

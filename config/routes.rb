b55c1825f27a2f11c4e807088ce4e4b1c2f1fb7c
Edward Schults <edward.schults@gmail.com>
2014-12-16 17:13:45 +0100

    conversations controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
 
+  get 'conversations/index'
+
+  get 'conversations/new'
+
+  get 'conversations/create'
+
+  get 'conversations/show'
+
+  get 'conversations/reply'
+
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "pages#home"
   resources :users, only: [:show, :edit, :update]
   resources :offers, except: [:destroy] do
     resources :bookings, only: [:new, :create, :edit, :update, :show]
+  end
+
+  resources :conversations, only: [:index, :new, :create, :show] do
+    member do
+      put :reply
+    end
   end
 
 end

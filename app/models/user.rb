ba7daea4d234184d012f1ed1a56030aaab5ca555
Edward Schults <edward.schults@gmail.com>
2014-12-16 13:11:11 +0100

    added message _form view

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,12 @@ class User < ActiveRecord::Base
   def name
     "#{self.first_name.capitalize} #{self.last_name.capitalize}"
   end
+
+  def mailboxer_email(object)
+    #Check if an email should be sent for that object
+    #if true
+    email
+    #if false
+    #return nil
+  end
 end

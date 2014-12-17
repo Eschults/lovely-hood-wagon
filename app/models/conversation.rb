723f0bba5b716c8a7c822a2a8d3ef2c8107243b2
Edward Schults <edward.schults@gmail.com>
2014-12-17 17:19:20 +0100

    added badges and navbar link

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -5,4 +5,18 @@ class Conversation < ActiveRecord::Base
   has_many :messages
   accepts_nested_attributes_for :messages
   validates_presence_of :user1, :user2
+
+  def other_user(current_user)
+    if user1 == current_user
+      user2
+    elsif user2 == current_user
+      user1
+    else
+      nil
+    end
+  end
+
+  def unread_messages(current_user)
+    messages.select{ |message| message.writer != current_user }.select { |message| message.read_at == nil }.size
+  end
 end

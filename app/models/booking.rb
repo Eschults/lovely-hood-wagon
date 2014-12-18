e2d630f6f348510983f2a88a3ee66e32c2e57766
Edward Schults <edward.schults@gmail.com>
2014-12-18 14:49:31 +0100

    mailer for bookings#update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,21 @@
 class Booking < ActiveRecord::Base
+  after_create :send_book_email
+  after_update :send_answer_email
+
   belongs_to :offer
   belongs_to :user
 
   validates_presence_of :offer, :user, :start_date
+
+  def send_book_email
+    BookingMailer.book(self).deliver
+  end
+
+  def send_answer_email
+    if self.accepted
+      BookingMailer.accept(self).deliver
+    else
+      BookingMailer.decline(self).deliver
+    end
+  end
 end

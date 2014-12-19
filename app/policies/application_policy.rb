0bd57b6204a851fb294495d79e1687b4414c7e3f
Edward Schults <edward.schults@gmail.com>
2014-12-19 13:25:59 +0100

    allowed destruction by admin

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -23,7 +23,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    false || user.admin
   end
 
   def edit?
@@ -31,7 +31,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    user.admin
   end
 
   def scope

cde3bd053ee34235a4b5d50d8f5fefe0061f3567
Edward Schults <edward.schults@gmail.com>
2014-12-22 12:43:36 +0100

    refactored js on users#edit and updated link_to my-trust

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,64 @@
+function showInfo() {
+  $('#user-info').removeClass("hidden");
+  $('#user-picture').addClass("hidden");
+  $('#user-trust').addClass("hidden");
+  $('#user-comments').addClass("hidden");
+  $('#user-recommendations').addClass("hidden");
+  $('#my-info').addClass("strong");
+  $('#my-pic').removeClass("strong");
+  $('#my-trust').removeClass("strong");
+  $('#my-comments').removeClass("strong");
+  $('#my-recos').removeClass("strong");
+}
+
+function showPic() {
+  $('#user-info').addClass("hidden");
+  $('#user-picture').removeClass("hidden");
+  $('#user-trust').addClass("hidden");
+  $('#user-comments').addClass("hidden");
+  $('#user-recommendations').addClass("hidden");
+  $('#my-info').removeClass("strong");
+  $('#my-pic').addClass("strong");
+  $('#my-trust').removeClass("strong");
+  $('#my-comments').removeClass("strong");
+  $('#my-recos').removeClass("strong");
+}
+
+function showTrust() {
+  $('#user-info').addClass("hidden");
+  $('#user-picture').addClass("hidden");
+  $('#user-trust').removeClass("hidden");
+  $('#user-comments').addClass("hidden");
+  $('#user-recommendations').addClass("hidden");
+  $('#my-info').removeClass("strong");
+  $('#my-pic').removeClass("strong");
+  $('#my-trust').addClass("strong");
+  $('#my-comments').removeClass("strong");
+  $('#my-recos').removeClass("strong");
+}
+
+function showComments() {
+  $('#user-info').addClass("hidden");
+  $('#user-picture').addClass("hidden");
+  $('#user-trust').addClass("hidden");
+  $('#user-comments').removeClass("hidden");
+  $('#user-recommendations').addClass("hidden");
+  $('#my-info').removeClass("strong");
+  $('#my-pic').removeClass("strong");
+  $('#my-trust').removeClass("strong");
+  $('#my-comments').addClass("strong");
+  $('#my-recos').removeClass("strong");
+}
+
+function showRecos() {
+  $('#user-info').addClass("hidden");
+  $('#user-picture').addClass("hidden");
+  $('#user-trust').addClass("hidden");
+  $('#user-comments').addClass("hidden");
+  $('#user-recommendations').removeClass("hidden");
+  $('#my-info').removeClass("strong");
+  $('#my-pic').removeClass("strong");
+  $('#my-trust').removeClass("strong");
+  $('#my-comments').removeClass("strong");
+  $('#my-recos').addClass("strong");
+}

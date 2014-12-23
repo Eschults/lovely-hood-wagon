900386fc65ddc9267f8078eddbc47f0260287f9d
Edward Schults <edward.schults@gmail.com>
2014-12-23 12:09:18 +0100

    removed dual models for Review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < ApplicationController
+  before_action :set_offer, only: [:show, :edit, :update]
+
   def new
   end
 
@@ -6,5 +8,15 @@ class ReviewsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def set_review
+
   end
 end

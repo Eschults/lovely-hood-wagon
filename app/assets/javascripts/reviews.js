ba8133b065a2ed00b28ce808bfc352617ec2ca3d
Edward Schults <edward.schults@gmail.com>
2014-12-24 18:17:00 +0100

    star-rating system operational

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,11 +1,51 @@
 function highlightStars(star) {
   var id = star.attr('id');
-  var index = parseInt(id[id.length - 1]);
-  id = id.slice(0, -1);
-  for(i = 1; i <= index; i++) {
-    $('#' + id + i).removeClass('rating-star').addClass('rating-star-highlight');
+  var stars = parseInt(id[id.length - 1]);
+  id = id.slice(0, -2);
+
+  for(i = 1; i <= stars; i++) {
+    $('#' + id + '_' + i).removeClass('rating-star').addClass('rating-star-highlight');
   }
-  for(i = index + 1; i <= 5; i++) {
-    $('#' + id + i).addClass('rating-star').removeClass('rating-star-highlight');
+  for(i = stars + 1; i <= 5; i++) {
+    $('#' + id + '_' + i).addClass('rating-star').removeClass('rating-star-highlight');
+  }
+
+  setStarValue(id, stars);
+
+  $.ajax({
+    type: "PUT",
+    url: $('#edit-review').attr('action'),
+    data: $('#edit-review').serialize()
+  });
+}
+
+function highlightThumbs(thumbs) {
+  var id = thumbs.attr('id');
+  if(id == "thumbs-up") {
+    $('#thumbs-up').removeClass('recommendation-thumbs').addClass('recommendation-thumbs-highlight');
+    $('#thumbs-down').addClass('recommendation-thumbs').removeClass('recommendation-thumbs-highlight');
+  } else {
+    $('#thumbs-up').addClass('recommendation-thumbs').removeClass('recommendation-thumbs-highlight');
+    $('#thumbs-down').removeClass('recommendation-thumbs').addClass('recommendation-thumbs-highlight');
+  }
+
+  setThumbsValue(id);
+
+  $.ajax({
+    type: "PUT",
+    url: $('#edit-review').attr('action'),
+    data: $('#edit-review').serialize()
+  });
+}
+
+function setStarValue(id, stars) {
+  $('#' + id).val(stars);
+}
+
+function setThumbsValue(id) {
+  if(id == 'thumbs-up') {
+    $('#recommendation').val(true);
+  } else if(id == 'thumbs-down') {
+    $('#recommendation').val(false);
   }
 }

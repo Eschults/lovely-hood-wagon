24c6baad93fe303a9b438ab7d2468891a1ca5cbc
Edward Schults <edward.schults@gmail.com>
2014-12-26 18:19:59 +0100

    finished reviews display on users#show and offers#show

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ActiveRecord::Base
   belongs_to :booking
 
   validates_presence_of :booking, :comment, :review_type
+
+  def cto_score
+    (communication_rating + punctuality_rating + quality_price_rating).fdiv(3)
+  end
 end

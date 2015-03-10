306244eb1973abaa853924d2e885e43660983fbd
Amelie Beurrier <abeurrier@gmail.com>
2015-03-10 13:36:18 +0100

    stripe: cashin process ok --added views for stripe_payments#new and #create

--- app/controllers/stripe_payments_controller.rb
+++ app/controllers/stripe_payments_controller.rb
@@ -8,13 +8,19 @@ class StripePaymentsController < ApplicationController
     @user = @booking.user
     customer = @user.stripe_customer
 
-    charge = Stripe::Charge.create(
-      :amount => @booking.cashin_price.to_i, # amount in cents, again
-      :currency => "eur",
-      :customer => customer.id,
-      :description => "#{@user.email} : booking n°#{@booking.id}"
-    )
-    @booking.cashed_in = true
+    if @booking.accepted
+
+      charge = Stripe::Charge.create(
+        :amount => @booking.cashin_price.to_i, # amount in cents, again
+        :currency => "eur",
+        :customer => customer.id,
+        :description => "#{@user.email} : booking n°#{@booking.id}"
+      )
+      @booking.cashed_in = true
+    else
+      flash[:notice] = "Ce booking n'a pas été accepté mec pay attention!"
+      render :new
+    end
   rescue Stripe::CardError => e
   end
 end

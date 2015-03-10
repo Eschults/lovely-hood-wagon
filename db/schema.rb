f046339bf1484275aaf7a5d9a7f6e3b5baf7b152
Amelie Beurrier <abeurrier@gmail.com>
2015-03-10 12:07:53 +0100

    added stripe_customer_token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203125344) do
+ActiveRecord::Schema.define(version: 20150310110704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -159,6 +159,7 @@ ActiveRecord::Schema.define(version: 20150203125344) do
     t.datetime "address_proof_updated_at"
     t.boolean  "admin"
     t.string   "street"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

55a439b1ef1f923a3f75f6743e629bedd4164306
Eschults <edward.schults@gmail.com>
2015-03-10 13:08:35 +0100

    removed irrelevant routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       end
     end
     resources :conversations, only: [:new, :create]
-    resources :stripe_customers
+    resources :stripe_customers, only: [:new, :create]
   end
 
   get 'bookings/:booking_id/reviews/new', to: "reviews#new", as: "new_booking_review"

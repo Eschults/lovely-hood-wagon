8757c190966c76273e4061e8091ba22fbb4a77f9
Amelie Beurrier <abeurrier@gmail.com>
2015-03-12 18:27:27 +0100

    removed syntax error

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -10,10 +10,10 @@ class ReviewPolicy < ApplicationPolicy
   end
 
   def create?
-    user.authorized && (record.booking.offer.user == user || record.booking.user == user  # Anyone can create a booking)
+    user.authorized && (record.booking.offer.user == user || record.booking.user == user)  # Anyone can create a booking)
   end
 
   def update?
-    user.authorized && (record.booking.offer.user == user || record.booking.user == user  # Anyone can create a booking)
+    user.authorized && (record.booking.offer.user == user || record.booking.user == user)  # Anyone can create a booking)
   end
 end

46a299ed142193808b589272b310f7500d5945ff
Eschults <edward.schults@gmail.com>
2015-03-15 17:28:36 +0100

    added Offer to ActiveAdmin dashboard

---
+++ app/admin/offer.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Offer do
+  index do
+    selectable_column
+    column :id
+    column :user_id
+    column :nature
+    column :type_of_offer
+    column :hourly_price
+    column :weekly_price
+    column :daily_price
+    column :published
+    column :guarantee
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  permit_params :published, :sold
+end

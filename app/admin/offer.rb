81ffb9db55943b0d9b53787b7866c34c4a5dfa8d
Eschults <edward.schults@gmail.com>
2015-03-15 17:49:23 +0100

    replaced :user_id by :user in admin/offers

--- app/admin/offer.rb
+++ app/admin/offer.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Offer do
   index do
     selectable_column
     column :id
-    column :user_id
+    column :user
     column :nature
     column :type_of_offer
     column :hourly_price

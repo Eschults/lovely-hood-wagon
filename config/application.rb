675b4148e056294b285c11fc253368243ba37240
Amelie Beurrier <abeurrier@gmail.com>
2015-03-26 12:04:38 -0400

    added i18n

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module LovelyHoodWagon
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -25,6 +27,6 @@ module LovelyHoodWagon
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fr
   end
 end

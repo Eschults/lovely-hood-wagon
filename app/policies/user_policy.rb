bc7a1ae25951d07228c351b94c82cb8d50cb13a1
Eschults <edward.schults@gmail.com>
2015-03-25 21:17:59 -0400

    enlarged neighbors scope for admins

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,7 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      if user.admin
+      if user.admin?
         scope.all
       else
         scope.where("(latitude - :my_lat) * (latitude - :my_lat) + (longitude - :my_lng) * (longitude - :my_lng) < 0.004 * 0.004 AND id != :my_id", my_lat: user.latitude, my_lng: user.longitude, my_id: user.id)

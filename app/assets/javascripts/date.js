a1d9152e46cc813ec0264d2d642b790b1cb8c5a1
Eschults <edward.schults@gmail.com>
2015-04-16 12:53:02 +0200

    added pricing details in bookings#new

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -47,7 +47,7 @@ function setEndDate(type) {
     var startDate = Date.parse($(this).val().slice(6, 10) + '-' + $(this).val().slice(3, 5) + '-' + $(this).val().slice(0, 2));
     if(type == "rent") {
       var endDate = startDate + 1 * 86400000;
-    } else {
+    } else if(type == "sell") {
       var endDate = startDate;
     }
     endDate = new Date(endDate).toString();
@@ -56,8 +56,133 @@ function setEndDate(type) {
     endDay = day(endDate);
     endDate = endDay + "/" + endMonth + "/" +endYear;
     endDateJs = endYear + "-" + endMonth + "-" +endDay;
-    $('#end-date').val(endDate);
+    if(type == "rent" || type == "sell") {
+      $('#end-date').val(endDate);
+    }
   })
+}
+
+function setDays() {
+  $('#start-date').on('change', function() {
+    var startDate = Date.parse($(this).val().slice(6, 10) + '-' + $(this).val().slice(3, 5) + '-' + $(this).val().slice(0, 2));
+    var endDate = Date.parse($('#end-date').val().slice(6, 10) + '-' + $('#end-date').val().slice(3, 5) + '-' + $('#end-date').val().slice(0, 2));
+    var nbDays = (endDate - startDate) / 86400000;
+    var total
+    $('#renting-days').text(nbDays);
+    if(nbDays > 6) {
+      $('#renting-days-equation').text(nbDays + " / 7");
+      $('#renting-days-sentence').text(nbDays);
+      $('#daily-price').removeClass('strong');
+      $('#weekly-price').addClass('strong');
+      $('#renting-price').text($('#weekly-price').text().split("€")[0])
+      total = parseInt(nbDays) / 7 * parseInt($('#weekly-price').text().split("€")[0]) * 1.08;
+      $('#renting-total').text(Math.floor(total))
+    } else {
+      $('#renting-days-equation').text(nbDays);
+      $('#renting-days-sentence').text(nbDays);
+      $('#daily-price').addClass('strong');
+      $('#weekly-price').removeClass('strong');
+      $('#renting-price').text($('#daily-price').text().split("€")[0])
+      total = parseInt(nbDays) * parseInt($('#daily-price').text().split("€")[0]) * 1.08;
+      $('#renting-total').text(Math.floor(total))
+    }
+  })
+  $('#end-date').on('change', function() {
+    var endDate = Date.parse($(this).val().slice(6, 10) + '-' + $(this).val().slice(3, 5) + '-' + $(this).val().slice(0, 2));
+    var startDate = Date.parse($('#start-date').val().slice(6, 10) + '-' + $('#start-date').val().slice(3, 5) + '-' + $('#start-date').val().slice(0, 2));
+    var nbDays = (endDate - startDate) / 86400000;
+    $('#renting-days').text(nbDays);
+    if(nbDays > 6) {
+      $('#renting-days-equation').text(nbDays + " / 7");
+      $('#renting-days-sentence').text(nbDays);
+      $('#daily-price').removeClass('strong');
+      $('#weekly-price').addClass('strong');
+      $('#renting-price').text($('#weekly-price').text().split("€")[0])
+      total = parseInt(nbDays) / 7 * parseInt($('#weekly-price').text().split("€")[0]) * 1.08;
+      $('#renting-total').text(Math.floor(total))
+    } else {
+      $('#renting-days-equation').text(nbDays);
+      $('#renting-days-sentence').text(nbDays);
+      $('#daily-price').addClass('strong');
+      $('#weekly-price').removeClass('strong');
+      $('#renting-price').text($('#daily-price').text().split("€")[0])
+      total = parseInt(nbDays) * parseInt($('#daily-price').text().split("€")[0]) * 1.08;
+      $('#renting-total').text(Math.floor(total))
+    }
+  })
+}
+
+
+function setHours() {
+  var startHour = $('#booking_start_hour_4i').val();
+  var startMin = $('#booking_start_hour_5i').val();
+  var startDateTime = Date.parse($('#start-date').val().slice(6, 10) + '-' + $('#start-date').val().slice(3, 5) + '-' + $('#start-date').val().slice(0, 2));
+  startDateTime = new Date(startDateTime)
+  startDateTime.setHours(parseInt(startHour));
+  startDateTime.setMinutes(parseInt(startMin));
+
+  var endHour = $('#booking_end_hour_4i').val();
+  var endMin = $('#booking_end_hour_5i').val();
+  var endDateTime = Date.parse($('#end-date').val().slice(6, 10) + '-' + $('#end-date').val().slice(3, 5) + '-' + $('#end-date').val().slice(0, 2));
+  endDateTime = new Date(endDateTime)
+  endDateTime.setHours(parseInt(endHour));
+  endDateTime.setMinutes(parseInt(endMin));
+
+  var serviceHours = (endDateTime - startDateTime) / 3600000
+
+  function serviceResult() {
+    serviceHours = (endDateTime - startDateTime) / 3600000
+    $('#service-hours').text(serviceHours);
+    $('#service-hours-sentence').text(serviceHours);
+    var total = serviceHours * parseInt($('#service-price').text()) * 1.08
+    $('#service-total').text(Math.floor(total))
+    if(serviceHours <= 0) {
+      $('.btn').addClass('disabled')
+    } else {
+      $('.btn').removeClass('disabled')
+    }
+  }
+
+  $('#booking_start_hour_4i').on('change', function() {
+    startHour = $('#booking_start_hour_4i').val();
+    startDateTime.setHours(parseInt(startHour));
+    serviceResult()
+  })
+
+  $('#booking_start_hour_5i').on('change', function() {
+    startMin = $('#booking_start_hour_5i').val();
+    startDateTime.setMinutes(parseInt(startMin));
+    serviceResult()
+  })
+
+  $('#booking_end_hour_4i').on('change', function() {
+    endHour = $('#booking_end_hour_4i').val();
+    endDateTime.setHours(parseInt(endHour));
+    serviceResult()
+  })
+
+  $('#booking_end_hour_5i').on('change', function() {
+    endMin = $('#booking_end_hour_5i').val();
+    endDateTime.setMinutes(parseInt(endMin));
+    serviceResult()
+  })
+
+  $('#start-date').on('change', function() {
+    startDateTime = Date.parse($('#start-date').val().slice(6, 10) + '-' + $('#start-date').val().slice(3, 5) + '-' + $('#start-date').val().slice(0, 2));
+    startDateTime = new Date(startDateTime)
+    startDateTime.setHours(parseInt(startHour));
+    startDateTime.setMinutes(parseInt(startMin));
+    serviceResult()
+  })
+
+  $('#end-date').on('change', function() {
+    endDateTime = Date.parse($('#end-date').val().slice(6, 10) + '-' + $('#end-date').val().slice(3, 5) + '-' + $('#end-date').val().slice(0, 2));
+    endDateTime = new Date(endDateTime)
+    endDateTime.setHours(parseInt(endHour));
+    endDateTime.setMinutes(parseInt(endMin));
+    serviceResult()
+  })
+
 }
 
 function setStartDates(type) {

8784e1bd2f2859013a77f95060b37c6011f6accb
Eschults <edward.schults@gmail.com>
2015-05-15 11:14:47 +0200

    authorization >> default: true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427220028) do
+ActiveRecord::Schema.define(version: 20150515083500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,12 +133,12 @@ ActiveRecord::Schema.define(version: 20150427220028) do
   add_index "reviews", ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                       default: "", null: false
-    t.string   "encrypted_password",          default: "", null: false
+    t.string   "email",                       default: "",   null: false
+    t.string   "encrypted_password",          default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",               default: 0,  null: false
+    t.integer  "sign_in_count",               default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -182,7 +182,7 @@ ActiveRecord::Schema.define(version: 20150427220028) do
     t.string   "stripe_customer_token"
     t.string   "iban"
     t.string   "bic"
-    t.boolean  "authorized"
+    t.boolean  "authorized",                  default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

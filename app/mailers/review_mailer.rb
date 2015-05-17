29c63eaca86b61b96044d465f68344fa4dd1eb0e
Eschults <edward.schults@gmail.com>
2015-05-17 21:15:43 +0200

    changed sender in mailers

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,5 +1,5 @@
 class ReviewMailer < ActionMailer::Base
-  default from: '"Edward Schults" <contact@lovely-hood.com>'
+  default from: '"Lovely Hood" <contact@lovely-hood.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:

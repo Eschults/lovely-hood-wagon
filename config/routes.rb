470ba7b916c5b233acbc22c3dbea74c095f06f16
Edward Schults <edward.schults@gmail.com>
2015-09-21 17:37:08 +0200

    testing with Claire -- bug fixing #36

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,10 @@ Rails.application.routes.draw do
   patch 'conversations/:id/reply', to: "conversations#reply", as: "reply_conversation_2"
   put 'conversations/:id/reply_server', to: "conversations#reply_server", as: "reply_server_conversation"
   patch 'conversations/:id/reply_server', to: "conversations#reply_server", as: "reply_server_conversation_2"
-  put 'bookings/:id/buy', to: "bookings#buy", as: "buy_booking"
   patch 'bookings/:id/buy', to: "bookings#buy", as: "buy_booking"
-  put 'bookings/:id/cancel', to: "bookings#cancel", as: "cancel_booking"
+  put 'bookings/:id/buy', to: "bookings#buy", as: "buy_booking_2"
   patch 'bookings/:id/cancel', to: "bookings#cancel", as: "cancel_booking"
+  put 'bookings/:id/cancel', to: "bookings#cancel", as: "cancel_booking_2"
   scope '(:locale)', locale: /en/ do
     root to: "pages#home"
     get "/legal", to: "pages#legal"

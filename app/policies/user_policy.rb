455643bea5c832b9411162ae59296bd81d576f6b
Edward Schults <edward.schults@gmail.com>
2015-09-28 16:16:48 +0200

    reordered neighbors

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,11 +2,11 @@ class UserPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       if user.admin?
-        scope.all.reject { |u| u.id == user.id }
+        scope.all.reject { |u| u.id == user.id }.sort_by(&:updated_at).reverse
       elsif user.latitude.nil?
         scope.all.reject { |u| u.id }
       else
-        scope.all.reject { |u1| u1.latitude.nil? || u1.id == user.id }.select { |u| user.is_distant_in_km_from(u) <= 1.1 }
+        scope.all.reject { |u1| u1.latitude.nil? || u1.id == user.id }.select { |u| user.is_distant_in_km_from(u) <= 1.1 }.sort_by(&:updated_at).reverse
       end
     end
   end

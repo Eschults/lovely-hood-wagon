db4a72d82132d2e70da4c9daacd8d624f1f3bd3a
Edward Schults <edward.schults@gmail.com>
2015-11-06 08:32:57 +0100

    added rememberable to omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
   def facebook
     user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
+      remember_me(user)
       # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]

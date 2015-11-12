dcc1aac015cc73eb118f4662346b3a1b83e33865
Edward Schults <edward.schults@gmail.com>
2015-11-12 13:25:59 +0100

    added seed and updated welcome email

---
+++ db/seeds.rb
@@ -0,0 +1,252 @@
+camille       = User.create(
+                  first_name: "Camille",
+                  last_name: "Renard",
+                  email: "camille.renard@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+pierre        = User.create(
+                  first_name: "Pierre",
+                  last_name: "Rolland",
+                  email: "pierre.rolland@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+marie         = User.create(
+                  first_name: "Marie",
+                  last_name: "PIETRUSZKA".downcase.capitalize,
+                  email: "marie.pietruszka@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+amaury        = User.create(
+                  first_name: "Amaury",
+                  last_name: "VACHER".downcase.capitalize,
+                  email: "amaury.vacher@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+clement       = User.create(
+                  first_name: "Clément",
+                  last_name: "WITZ".downcase.capitalize,
+                  email: "clement.witz@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+lucas         = User.create(
+                  first_name: "Lucas",
+                  last_name: "FAUROUX".downcase.capitalize,
+                  email: "lucas.fauroux@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+mohamed       = User.create(
+                  first_name: "Mohamed",
+                  last_name: "ANDALOUS-OUARTASSI".downcase.capitalize,
+                  email: "mohamed.andalous-ouartassi@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+paul          = User.create(
+                  first_name: "Paul Adrien",
+                  last_name: "JACQUES".downcase.capitalize,
+                  email: "paul-adrien.jacques@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+hamza         = User.create(
+                  first_name: "Hamza",
+                  last_name: "IRAQI".downcase.capitalize,
+                  email: "hamza.iraqi@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+orlane        = User.create(
+                  first_name: "Orlane",
+                  last_name: "AZOGUI".downcase.capitalize,
+                  email: "orlane.azogui@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+laurie        = User.create(
+                  first_name: "Laurie",
+                  last_name: "BONIN".downcase.capitalize,
+                  email: "laurie.bonin@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+lorene        = User.create(
+                  first_name: "Lorène",
+                  last_name: "PERNET".downcase.capitalize,
+                  email: "lorene.pernet@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+marine        = User.create(
+                  first_name: "Marine",
+                  last_name: "MARTIN".downcase.capitalize,
+                  email: "marine.martin@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+leo           = User.create(
+                  first_name: "Léo",
+                  last_name: "GONIN".downcase.capitalize,
+                  email: "leo.gonin@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+thierry       = User.create(
+                  first_name: "Thierry",
+                  last_name: "TRANCHANT".downcase.capitalize,
+                  email: "thierry.tranchant@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+benjamin      = User.create(
+                  first_name: "Benjamin",
+                  last_name: "KARILA".downcase.capitalize,
+                  email: "benjamin.karila@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+julie         = User.create(
+                  first_name: "Julie",
+                  last_name: "MARCHAND".downcase.capitalize,
+                  email: "julie.marchand@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+morgane       = User.create(
+                  first_name: "Morgane",
+                  last_name: "SUQUET".downcase.capitalize,
+                  email: "morgane.suquet@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+balthazar     = User.create(
+                  first_name: "Balthazar",
+                  last_name: "Villey-Desmeserets",
+                  email: "bvilleyd@gmail.com",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+younes        = User.create(
+                  first_name: "Younes",
+                  last_name: "BENSAID".downcase.capitalize,
+                  email: "younes.bensaid@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+jean_baptiste = User.create(
+                  first_name: "Jean Baptiste",
+                  last_name: "GUYOT".downcase.capitalize,
+                  email: "jean-baptiste.guyot@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+guillaume     = User.create(
+                  first_name: "Guillaume",
+                  last_name: "DESBONNETS".downcase.capitalize,
+                  email: "guillaume.desbonnets@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+martin        = User.create(
+                  first_name: "Martin",
+                  last_name: "MEFFRE".downcase.capitalize,
+                  email: "martin.meffre@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+hawai         = User.create(
+                  first_name: "Hawaï",
+                  last_name: "MAAREK".downcase.capitalize,
+                  email: "hawai.maarek@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+vianney       = User.create(
+                  first_name: "Vianney",
+                  last_name: "HAËNTJENS".downcase.capitalize,
+                  email: "vianney.haentjens@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+clemence      = User.create(
+                  first_name: "Clémence",
+                  last_name: "DAVID".downcase.capitalize,
+                  email: "clemence.david1@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+marief        = User.create(
+                  first_name: "Marie",
+                  last_name: "FORGET".downcase.capitalize,
+                  email: "marie.forget@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )
+edouard       = User.create(
+                  first_name: "Edouard",
+                  last_name: "GUICHOUX".downcase.capitalize,
+                  email: "edouard.guichoux@hec.edu",
+                  password: "hec2015$",
+                  street: "1 Rue de la Libération",
+                  zip_code: "78350",
+                  city: "Jouy-en-Josas"
+                )

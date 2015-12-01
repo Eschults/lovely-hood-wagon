26c2e822b284135db09f4be1770ec2ff7670afdf
Edward Schults <edward.schults@gmail.com>
2015-12-01 14:34:05 +0100

    neighbors in react ok

--- app/assets/javascripts/components/neighbor_list.js.jsx
+++ app/assets/javascripts/components/neighbor_list.js.jsx
@@ -1,7 +1,7 @@
 var NeighborList = React.createClass({
   render: function() {
     return (
-      <div className="panel-body padding-none border-top-221">
+      <div className="panel-body padding-none" id="neighbors-stream">
         {this.props.neighbors.map(function(neighbor, index) {
           return <NeighborListItem neighbor={neighbor} key={index} />
         })}
--- app/assets/javascripts/components/neighbor_list_item.js.jsx
+++ app/assets/javascripts/components/neighbor_list_item.js.jsx
@@ -6,27 +6,33 @@ var NeighborListItem = React.createClass({
     var style = {
       backgroundImage: 'url(' + this.props.neighbor.picture + ')'
     }
+    var tooltip;
+    if(this.props.neighbor.description != "" && this.props.neighbor.description != null) {
+      tooltip = <OverlayTrigger placement="top" overlay={
+                  <Tooltip id={"tooltip_" + this.props.neighbor.id}>
+                    {this.props.neighbor.description}
+                  </Tooltip>
+                }>
+                  <a className="card-link" href={this.props.neighbor.neighbor_path}>
+                  </a>
+                </OverlayTrigger>
+    } else {
+      tooltip = <a className="card-link" href={this.props.neighbor.neighbor_path}></a>
+    }
     return (
       <div className="col-xs-6 col-sm-4">
         <div className="card">
           <span className="card-bg" style={style}></span>
           <span className="card-shadow"></span>
           <span className="card-description">
-            {this.props.neighbor.description}
+            {this.props.neighbor.offersInfo}
           </span>
           <span className="card-text">
             <h2>{this.props.neighbor.first_name}</h2>
           </span>
           <span className="card-user">
           </span>
-          <OverlayTrigger placement="top" overlay={
-            <Tooltip>
-              {this.props.neighbor.description}
-            </Tooltip>
-          }>
-            <a className="card-link" href={this.props.neighbor.neighbor_path}>
-            </a>
-          </OverlayTrigger>
+          {tooltip}
         </div>
       </div>
     )

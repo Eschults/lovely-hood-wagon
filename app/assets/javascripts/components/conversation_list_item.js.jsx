ad5fbabb86599d46099deeba843d7e4a1b5e6e8b
Edward Schults <edward.schults@gmail.com>
2015-12-02 18:42:25 +0100

    playing with preview icons depending on read state and current user

--- app/assets/javascripts/components/conversation_list_item.js.jsx
+++ app/assets/javascripts/components/conversation_list_item.js.jsx
@@ -1,34 +1,51 @@
 var ConversationListItem = React.createClass({
   getInitialState() {
     return {
-      read: this.props.conversation.read
+      conversation: this.props.conversation,
     };
   },
   render: function() {
+    var read = this.state.conversation.read
+    var isSenderCurrentUser = this.props.conversation.is_last_message_sender_current_user
     var faClasses = classNames({
       "fa": true,
       "gray-lighter": true,
-      "fa-check": this.state.read,
-      "fa-reply": !this.state.read
+      "fa-check": read,
+      "fa-reply": !read
     })
+    var badgeClasses = classNames({
+      "badge": true,
+      "small-badge": true,
+      "small-badge-off": read
+    })
+    var lastMessageInfo
+    if(isSenderCurrentUser) {
+      lastMessageInfo = <i className={faClasses}></i>&nbsp;&nbsp;
+    } else {
+      lastMessageInfo = <span className={badgeClasses}> </span>
+    }
+
     return(
       <div className="conversation-preview-link" onClick={this.handleClick} key='test'>
         <div className="conversation-preview" id={"conversation_" + this.props.conversation.id}>
           <div className="col-xs-3 img-medium-square padding-none">
             <img src={this.props.conversation.picture} className="img"/>
           </div>
-          <div className="col-xs-7 overflow-hidden">
-            <h5 className="margin-btm-5 first-name"><strong>{this.props.conversation.first_name}</strong></h5>
+          <div className="col-xs-9 overflow-hidden padding-right-none">
+            <ul className="list-inline list-unstyled margin-btm-none">
+              <li><h5 className="margin-btm-5 first-name"><strong>{this.props.conversation.first_name}</strong></h5></li>
+              <li className="pull-right margin-top-10 padding-sides-none"><p className="small-o gray-lighter text-right">{this.props.conversation.last_message_created_at}</p></li>
+            </ul>
             <div className="one-line">
-              <i className={faClasses}></i>&nbsp;&nbsp;
+              {lastMessageInfo}
               <span dangerouslySetInnerHTML={{__html: this.props.conversation.last_content}}></span>
             </div>
-          </div>
-          <div className="col-xs-2 margin-top-10 padding-none">
-            <p className="small gray-lighter text-right margin-top-14">{this.props.conversation.last_message_created_at}</p>
           </div>
         </div>
       </div>
     )
+  },
+  handleClick: function() {
+
   }
 })
